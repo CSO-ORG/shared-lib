22eabe6885de8b7cc00f0db060678c0ca4694daa
KaymKassai <kassai.kaym@gmail.com>
2023-06-18 12:15:24 +0200

    Library update

--- types/enums.ts
+++ types/enums.ts
@@ -2,3 +2,15 @@ export enum ACCOUNT_TYPE {
   USER = "user",
   ADMIN = "admin",
 }
+
+export enum PET_TYPE {
+  cat = "cat",
+  dog = "dog",
+  bird = "bird",
+  turtle = "turtle",
+  rabbit = "rabbit",
+  cow = "cow",
+  horse = "horse",
+  sheep = "sheep",
+  goat = "goat",
+}
