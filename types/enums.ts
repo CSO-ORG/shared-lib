7bdd77d05dc2e000557ed723a7a5deb1465bef54
KaymKassai <kassai.kaym@gmail.com>
2023-08-17 19:56:57 +0200

    Library update

--- types/enums.ts
+++ types/enums.ts
@@ -16,3 +16,65 @@ export enum PET_TYPE {
   goat = "goat",
   other = "other",
 }
+
+export enum ALERT_STATUS {
+  IN_CREATION = "in_creation",
+  PUBLISHED = "published",
+  TERMINATED = "terminated",
+}
+
+export enum ALERT_CATEGORY {
+  LOST_PET = "lost_pet",
+  FOUND_PET = "found_pet",
+}
+
+export enum PET_CATEGORY {
+  DOG = "dog",
+  CAT = "cat",
+  BIRD = "bird",
+  FISH = "fish",
+  RABBIT = "rabbit",
+  HAMSTER = "hamster",
+  GUINEA_PIG = "guinea_pig",
+  REPTILE = "reptile",
+  OTHER = "other",
+}
+
+export enum SEX_CATEGORY {
+  HERMAPHRODITE = "hermaphrodite",
+  MALE = "male",
+  FEMALE = "female",
+}
+
+export enum HEIGHT_CATEGORY {
+  SHORT = "short",
+  AVERAGE = "average",
+  TALL = "tall",
+}
+
+export enum WEIGHT_CATEGORY {
+  THIN = "thin",
+  NORMAL = "normal",
+  PLUMP = "plump",
+}
+
+export enum HAIR_TYPE {
+  SHORT = "short",
+  LONG = "long",
+  FRIZZY = "frizzy",
+  SEMILONG = "semilong",
+  STRAIGHT = "straight",
+}
+
+export enum NECKLACE_MATERIAL_CATEGORY {
+  CHAIN = "chain",
+  LEATHER = "leather",
+  NYLON = "nylon",
+  PLASTIC = "plastic",
+}
+
+export enum CONTACT_TYPE {
+  PHONE = "phone",
+  EMAIL = "email",
+  WHATSAPP = "whatsapp",
+}
--- types/types.ts
+++ types/types.ts
@@ -2,3 +2,17 @@ export type PROFILE_PICTURE = {
   public_id: string;
   url: string;
 };
+
+export type DATETIME = {
+  date?: string;
+  time?: string;
+};
+
+export type LOCATION = {
+  country?: string;
+  city?: string;
+  address?: string;
+  postalCode?: string;
+  departmentName?: string;
+  departmentCode?: string;
+};
