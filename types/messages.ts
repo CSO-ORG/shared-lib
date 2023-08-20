6a4a150ca62b6875069dc6bd608d6455913b5ed1
KaymKassai <kassai.kaym@gmail.com>
2023-08-20 15:06:35 +0200

    Library update

--- types/messages.ts
+++ types/messages.ts
@@ -28,4 +28,9 @@ export enum ACCOUNT_SERVICE_MESSAGE {
 export enum GENERIC_MESSAGE {
   INTERNAL_SERVER_ERROR = "internal.server.error",
   MISSING_FIELDS = "missing.fields",
+  RESOURCE_CREATED = "resource.created",
+  RESOURCE_UPDATED = "resource.updated",
+  RESOURCE_DELETED = "resource.deleted",
+  RESOURCE_NOT_FOUND = "resource.not.found",
+  RESOURCE_FOUND = "resource.found",
 }
